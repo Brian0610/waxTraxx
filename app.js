c87708ce7f59ed3af93c9c8adaf44360706bfefb
Brian0610 <brian49345@gmail.com>
2015-09-26 23:26:24 -0400

    files added

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('#sign-up').hover(function(){
+		$(this).animate({ top: '-1em'}, 400); 
+		$
+	}, function(){
+		$(this).animate({height: '10em', top: '0em'}, 400);
+	}); 
+
+	$('#profile').hover(function(){
+		$(this).animate({ top: '-1em'}, 400); 
+	}, function(){
+		$(this).animate({ top: '0em'}, 400);
+	}); 
+
+
+
+
+}); 
